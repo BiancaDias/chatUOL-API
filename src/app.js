45baa5f2cda06aa2ac641d43bb6d3b11e84fe01f
Bianca Dias <bianca.dias@ufv.br>
2023-04-13 17:28:31 -0300

    validações com joi

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb";
 import dayjs from 'dayjs';
+import joi from 'joi';
 
 const app = express();
 app.listen(5000, console.log("servidor iniciado na porta 5000"))
@@ -18,46 +19,79 @@ mongoCilent.connect()
     .catch(err => console.log(err.message));
 
 app.post("/participants", (req, res) => {
-    db.collection("participants").find().toArray()
-        .then((participant) => {
-            if (participant.map(p => p.name).includes(req.body.name)) {
-                return res.sendStatus(409);
-            } else {
-                const participant = { name: req.body.name, lastStatus: Date.now() };
+    const name = req.body;
+    const userSchema = joi.object({name: joi.string().min(1).required()});
+    const validation = userSchema.validate(name, { abortEarly: false });
+    console.log(name)
+    if(validation.error){ //verificando se a string é vazia
+        return res.sendStatus(422);
+    }
+    const nameOutObject = req.body.name
+    db.collection("participants").findOne({name: nameOutObject})
+        .then((data) => {
+            if(data){
+                return res.sendStatus(409) //se o nome ja existe
+            }else{
+                const participant = { name: nameOutObject, lastStatus: Date.now() };
                 db.collection("participants").insertOne(participant)
                     .then(() => {
-                        res.sendStatus(201) //este é da inserção nos nomes de participantes
-                        db.collection("messages").insertOne({from: req.body.name, to: "Todos", text: "entra na sala...", type: 'status', time: dayjs().format('HH:mm:ss')})
+                        db.collection("messages").insertOne({from: nameOutObject, to: "Todos", text: "entra na sala...", type: 'status', time: dayjs().format('HH:mm:ss')})
                             .then(()=>res.sendStatus(201) ) //este é da mensagem
-                            .catch(err => res.status(500).send(err.message));
+                            .catch(err => res.sendStatus(500));
                     })
-                    .catch(err => res.status(500).send(err.message));
-            }
-        }).catch(err => {
-            return res.status(500).send(err.message)
-        });
-})
-app.get("/participants", (req, res) =>{
-    db.collection("participants").find().toArray()
-        .then(participant => res.send(participant))
-        .catch(err => res.status(500).send(err.message));
-})
-
-app.post("/messages", (req, res) =>{
-    const from = req.header.user;
-    const {to, text, type} = req.body //validar ainda com joi
-
-    db.collection("participants").find().toArray()
-        .then((participant) => {
-            if (participant.map(p => p.name).includes(from)) {
-                db.collection("messages").insertOne({from, to, text, type,time: dayjs().format('HH:mm:ss')})
-                    .then(()=>res.sendStatus(201) ) //este é da mensagem
-                    .catch(err => res.status(500).send(err.message));
-                return res.sendStatus(201);//este é da validação
-            }
-            else{
-                return res.sendStatus(422)
+                    .catch(err => res.sendStatus(500));
             }
         })
-        .catch(err => res.status(500).send(err.message));
+        .catch(err => res.sendStatus(500));
 })
+// app.get("/participants", (req, res) =>{
+//     db.collection("participants").find().toArray()
+//         .then(participant => res.send(participant))
+//         .catch(err => res.status(500).send(err.message));
+// })
+
+// app.post("/messages", (req, res) =>{
+//     const from = req.header.user;
+//     const {to, text, type} = req.body //validar ainda com joi
+
+//     db.collection("participants").find().toArray()
+//         .then((participant) => {
+//             if (participant.map(p => p.name).includes(from)) {
+//                 db.collection("messages").insertOne({from, to, text, type,time: dayjs().format('HH:mm:ss')})
+//                     .then(()=>res.sendStatus(201) ) //este é da mensagem
+//                     .catch(err => res.status(500).send(err.message));
+//                 return res.sendStatus(201);//este é da validação
+//             }
+//             else{
+//                 return res.sendStatus(422)
+//             }
+//         })
+//         .catch(err => res.status(500).send(err.message));
+// })
+
+// app.get("/messages", (req, res) => {
+//     const user = req.header.user;
+//     const limit = req.query;
+//     const quant =Number(limit);
+//     if(limit){
+//         if(/^\d+$/.test(limit)){
+//             if(quant === 0 || quant < 0) return res.send(422)
+//         }else{
+//             res.send(422)
+//         }
+//     }
+//     db.collection("messages").find({
+//         $and: [
+//           { $or: [{ from: user }, { to: user }] },
+//           { to: "Todos" }
+//         ]
+//       })
+//       .then((messages) =>{
+//         if(limit){
+//             const last100messages = messages.slice(-1 * quant)
+//             return res.send(last100messages)
+//         }
+//         return res.send(messages)
+//       })
+//       .catch(err => res.status(500).send(err.message));
+// })
